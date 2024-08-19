ab6361ad13ff90c7652e7a132e477e2aa2a17507
lazykapo <sideorder921@gmail.com>
2024-08-19 12:53:52 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(content: plan_params[:content])
     redirect_to action: :index
   end
 
